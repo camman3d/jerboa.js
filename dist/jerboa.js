d7d42eb9f41f20c4e565839978baa382e2a99522
david.chen <david.chen@experticity.com>
2016-12-12 13:42:08 -0800

    Add webpack-built jerboa.*

--- dist/jerboa.js
+++ dist/jerboa.js
@@ -210,26 +210,29 @@
 	    buttonContainer.style.left = left;
 	    buttonContainer.style.position = 'fixed';
 
-	    buttonDiv.addEventListener('click', function (event) {
-	        event.preventDefault();
+	    var matchState = function matchState() {
 	        var feedbackSpots = document.getElementsByClassName('feedback-spot');
-	        if (buttonDiv.classList.contains('toggle-button-selected')) {
-	            buttonDiv.classList.remove('toggle-button-selected');
-	            buttonLabel.textContent = 'Feedback Off';
-	            _state2.default.active = false;
-	            Array.prototype.forEach.call(feedbackSpots, function (feedbackSpotElement) {
-	                feedbackSpotElement.classList.add('off');
-	            });
-	        } else {
+	        if (_state2.default.active) {
 	            buttonDiv.classList.add('toggle-button-selected');
 	            buttonLabel.textContent = 'Feedback On';
-	            _state2.default.active = true;
 	            Array.prototype.forEach.call(feedbackSpots, function (feedbackSpotElement) {
 	                feedbackSpotElement.classList.remove('off');
 	            });
+	        } else {
+	            buttonDiv.classList.remove('toggle-button-selected');
+	            buttonLabel.textContent = 'Feedback Off';
+	            Array.prototype.forEach.call(feedbackSpots, function (feedbackSpotElement) {
+	                feedbackSpotElement.classList.add('off');
+	            });
 	        }
+	    };
+	    buttonDiv.addEventListener('click', function (event) {
+	        event.preventDefault();
+	        _state2.default.active = !_state2.default.active;
+	        matchState();
+	        (0, _events.emit)('active', _state2.default.active);
 	    });
-
+	    matchState();
 	    document.body.appendChild(buttonContainer);
 	}
 
@@ -262,6 +265,7 @@
 	    init: function init(options) {
 	        console.log('options', options);
 	        options = options || {};
+	        _state2.default.active = options.active || false;
 	        _state2.default.currentStrategy = options.strategy || strategies.global;
 	        _state2.default.currentPositioning = options.positioning || 'PERCENT';
 	        _state2.default.currentUser = options.currentUser;
@@ -269,6 +273,7 @@
 	        _state2.default.isAdmin = options.isAdmin || false;
 	        _state2.default.url = window.location.href;
 	        _state2.default.pageId = (0, _blueimpMd2.default)(window.location.href);
+	        _state2.default.allowDeleteComments = options.allowDeleteComments || false;
 	        console.log('state', _state2.default);
 	        if (options.data) {
 	            _state2.default.additionalData = options.data;
@@ -571,15 +576,23 @@
 	    text.appendChild(info);
 
 	    if (parseInt(payload.userId) === parseInt(_state2.default.currentUserId)) {
-	        var deleteBtn = document.createElement('a');
-	        deleteBtn.classList.add('delete-button');
-	        deleteBtn.innerText = 'X';
-	        deleteBtn.setAttribute('role', 'button');
-	        deleteBtn.setAttribute('href', '#');
-	        // don't render delete button for original annotation comment
-	        if (!payload.hasOwnProperty('comments')) {
-	            info.appendChild(deleteBtn);
-	        };
+	        if (_state2.default.allowDeleteComments) {
+	            var deleteBtn = document.createElement('a');
+	            deleteBtn.classList.add('delete-button');
+	            deleteBtn.innerText = 'X';
+	            deleteBtn.setAttribute('role', 'button');
+	            deleteBtn.setAttribute('href', '#');
+	            // don't render delete button for original annotation comment
+	            if (!payload.hasOwnProperty('comments')) {
+	                info.appendChild(deleteBtn);
+	            };
+
+	            deleteBtn.addEventListener('click', function (e) {
+	                e.preventDefault();
+	                container.removeChild(text);
+	                (0, _events.emit)('deleteComment', payload);
+	            });
+	        }
 
 	        var editBtn = document.createElement('a');
 	        editBtn.classList.add('edit-button');
@@ -587,12 +600,6 @@
 	        editBtn.setAttribute('role', 'button');
 	        editBtn.setAttribute('href', '#');
 	        info.appendChild(editBtn);
-
-	        deleteBtn.addEventListener('click', function (e) {
-	            e.preventDefault();
-	            container.removeChild(text);
-	            (0, _events.emit)('deleteComment', payload);
-	        });
 
 	        editBtn.addEventListener('click', function (e) {
 	            e.preventDefault();
@@ -967,7 +974,7 @@
 	 * See http://pajhome.org.uk/crypt/md5 for more info.
 	 */
 
-	/*global unescape, define, module */
+	/* global define */
 
 	;(function ($) {
 	  'use strict'
@@ -976,7 +983,7 @@
 	  * Add integers, wrapping at 2^32. This uses 16-bit operations internally
 	  * to work around bugs in some JS interpreters.
 	  */
-	  function safe_add (x, y) {
+	  function safeAdd (x, y) {
 	    var lsw = (x & 0xFFFF) + (y & 0xFFFF)
 	    var msw = (x >> 16) + (y >> 16) + (lsw >> 16)
 	    return (msw << 16) | (lsw & 0xFFFF)
@@ -985,33 +992,33 @@
 	  /*
 	  * Bitwise rotate a 32-bit number to the left.
 	  */
-	  function bit_rol (num, cnt) {
+	  function bitRotateLeft (num, cnt) {
 	    return (num << cnt) | (num >>> (32 - cnt))
 	  }
 
 	  /*
 	  * These functions implement the four basic operations the algorithm uses.
 	  */
-	  function md5_cmn (q, a, b, x, s, t) {
-	    return safe_add(bit_rol(safe_add(safe_add(a, q), safe_add(x, t)), s), b)
+	  function md5cmn (q, a, b, x, s, t) {
+	    return safeAdd(bitRotateLeft(safeAdd(safeAdd(a, q), safeAdd(x, t)), s), b)
 	  }
-	  function md5_ff (a, b, c, d, x, s, t) {
-	    return md5_cmn((b & c) | ((~b) & d), a, b, x, s, t)
+	  function md5ff (a, b, c, d, x, s, t) {
+	    return md5cmn((b & c) | ((~b) & d), a, b, x, s, t)
 	  }
-	  function md5_gg (a, b, c, d, x, s, t) {
-	    return md5_cmn((b & d) | (c & (~d)), a, b, x, s, t)
+	  function md5gg (a, b, c, d, x, s, t) {
+	    return md5cmn((b & d) | (c & (~d)), a, b, x, s, t)
 	  }
-	  function md5_hh (a, b, c, d, x, s, t) {
-	    return md5_cmn(b ^ c ^ d, a, b, x, s, t)
+	  function md5hh (a, b, c, d, x, s, t) {
+	    return md5cmn(b ^ c ^ d, a, b, x, s, t)
 	  }
-	  function md5_ii (a, b, c, d, x, s, t) {
-	    return md5_cmn(c ^ (b | (~d)), a, b, x, s, t)
+	  function md5ii (a, b, c, d, x, s, t) {
+	    return md5cmn(c ^ (b | (~d)), a, b, x, s, t)
 	  }
 
 	  /*
 	  * Calculate the MD5 of an array of little-endian words, and a bit length.
 	  */
-	  function binl_md5 (x, len) {
+	  function binlMD5 (x, len) {
 	    /* append padding */
 	    x[len >> 5] |= 0x80 << (len % 32)
 	    x[(((len + 64) >>> 9) << 4) + 14] = len
@@ -1032,78 +1039,78 @@
 	      oldc = c
 	      oldd = d
 
-	      a = md5_ff(a, b, c, d, x[i], 7, -680876936)
-	      d = md5_ff(d, a, b, c, x[i + 1], 12, -389564586)
-	      c = md5_ff(c, d, a, b, x[i + 2], 17, 606105819)
-	      b = md5_ff(b, c, d, a, x[i + 3], 22, -1044525330)
-	      a = md5_ff(a, b, c, d, x[i + 4], 7, -176418897)
-	      d = md5_ff(d, a, b, c, x[i + 5], 12, 1200080426)
-	      c = md5_ff(c, d, a, b, x[i + 6], 17, -1473231341)
-	      b = md5_ff(b, c, d, a, x[i + 7], 22, -45705983)
-	      a = md5_ff(a, b, c, d, x[i + 8], 7, 1770035416)
-	      d = md5_ff(d, a, b, c, x[i + 9], 12, -1958414417)
-	      c = md5_ff(c, d, a, b, x[i + 10], 17, -42063)
-	      b = md5_ff(b, c, d, a, x[i + 11], 22, -1990404162)
-	      a = md5_ff(a, b, c, d, x[i + 12], 7, 1804603682)
-	      d = md5_ff(d, a, b, c, x[i + 13], 12, -40341101)
-	      c = md5_ff(c, d, a, b, x[i + 14], 17, -1502002290)
-	      b = md5_ff(b, c, d, a, x[i + 15], 22, 1236535329)
+	      a = md5ff(a, b, c, d, x[i], 7, -680876936)
+	      d = md5ff(d, a, b, c, x[i + 1], 12, -389564586)
+	      c = md5ff(c, d, a, b, x[i + 2], 17, 606105819)
+	      b = md5ff(b, c, d, a, x[i + 3], 22, -1044525330)
+	      a = md5ff(a, b, c, d, x[i + 4], 7, -176418897)
+	      d = md5ff(d, a, b, c, x[i + 5], 12, 1200080426)
+	      c = md5ff(c, d, a, b, x[i + 6], 17, -1473231341)
+	      b = md5ff(b, c, d, a, x[i + 7], 22, -45705983)
+	      a = md5ff(a, b, c, d, x[i + 8], 7, 1770035416)
+	      d = md5ff(d, a, b, c, x[i + 9], 12, -1958414417)
+	      c = md5ff(c, d, a, b, x[i + 10], 17, -42063)
+	      b = md5ff(b, c, d, a, x[i + 11], 22, -1990404162)
+	      a = md5ff(a, b, c, d, x[i + 12], 7, 1804603682)
+	      d = md5ff(d, a, b, c, x[i + 13], 12, -40341101)
+	      c = md5ff(c, d, a, b, x[i + 14], 17, -1502002290)
+	      b = md5ff(b, c, d, a, x[i + 15], 22, 1236535329)
 
-	      a = md5_gg(a, b, c, d, x[i + 1], 5, -165796510)
-	      d = md5_gg(d, a, b, c, x[i + 6], 9, -1069501632)
-	      c = md5_gg(c, d, a, b, x[i + 11], 14, 643717713)
-	      b = md5_gg(b, c, d, a, x[i], 20, -373897302)
-	      a = md5_gg(a, b, c, d, x[i + 5], 5, -701558691)
-	      d = md5_gg(d, a, b, c, x[i + 10], 9, 38016083)
-	      c = md5_gg(c, d, a, b, x[i + 15], 14, -660478335)
-	      b = md5_gg(b, c, d, a, x[i + 4], 20, -405537848)
-	      a = md5_gg(a, b, c, d, x[i + 9], 5, 568446438)
-	      d = md5_gg(d, a, b, c, x[i + 14], 9, -1019803690)
-	      c = md5_gg(c, d, a, b, x[i + 3], 14, -187363961)
-	      b = md5_gg(b, c, d, a, x[i + 8], 20, 1163531501)
-	      a = md5_gg(a, b, c, d, x[i + 13], 5, -1444681467)
-	      d = md5_gg(d, a, b, c, x[i + 2], 9, -51403784)
-	      c = md5_gg(c, d, a, b, x[i + 7], 14, 1735328473)
-	      b = md5_gg(b, c, d, a, x[i + 12], 20, -1926607734)
+	      a = md5gg(a, b, c, d, x[i + 1], 5, -165796510)
+	      d = md5gg(d, a, b, c, x[i + 6], 9, -1069501632)
+	      c = md5gg(c, d, a, b, x[i + 11], 14, 643717713)
+	      b = md5gg(b, c, d, a, x[i], 20, -373897302)
+	      a = md5gg(a, b, c, d, x[i + 5], 5, -701558691)
+	      d = md5gg(d, a, b, c, x[i + 10], 9, 38016083)
+	      c = md5gg(c, d, a, b, x[i + 15], 14, -660478335)
+	      b = md5gg(b, c, d, a, x[i + 4], 20, -405537848)
+	      a = md5gg(a, b, c, d, x[i + 9], 5, 568446438)
+	      d = md5gg(d, a, b, c, x[i + 14], 9, -1019803690)
+	      c = md5gg(c, d, a, b, x[i + 3], 14, -187363961)
+	      b = md5gg(b, c, d, a, x[i + 8], 20, 1163531501)
+	      a = md5gg(a, b, c, d, x[i + 13], 5, -1444681467)
+	      d = md5gg(d, a, b, c, x[i + 2], 9, -51403784)
+	      c = md5gg(c, d, a, b, x[i + 7], 14, 1735328473)
+	      b = md5gg(b, c, d, a, x[i + 12], 20, -1926607734)
 
-	      a = md5_hh(a, b, c, d, x[i + 5], 4, -378558)
-	      d = md5_hh(d, a, b, c, x[i + 8], 11, -2022574463)
-	      c = md5_hh(c, d, a, b, x[i + 11], 16, 1839030562)
-	      b = md5_hh(b, c, d, a, x[i + 14], 23, -35309556)
-	      a = md5_hh(a, b, c, d, x[i + 1], 4, -1530992060)
-	      d = md5_hh(d, a, b, c, x[i + 4], 11, 1272893353)
-	      c = md5_hh(c, d, a, b, x[i + 7], 16, -155497632)
-	      b = md5_hh(b, c, d, a, x[i + 10], 23, -1094730640)
-	      a = md5_hh(a, b, c, d, x[i + 13], 4, 681279174)
-	      d = md5_hh(d, a, b, c, x[i], 11, -358537222)
-	      c = md5_hh(c, d, a, b, x[i + 3], 16, -722521979)
-	      b = md5_hh(b, c, d, a, x[i + 6], 23, 76029189)
-	      a = md5_hh(a, b, c, d, x[i + 9], 4, -640364487)
-	      d = md5_hh(d, a, b, c, x[i + 12], 11, -421815835)
-	      c = md5_hh(c, d, a, b, x[i + 15], 16, 530742520)
-	      b = md5_hh(b, c, d, a, x[i + 2], 23, -995338651)
+	      a = md5hh(a, b, c, d, x[i + 5], 4, -378558)
+	      d = md5hh(d, a, b, c, x[i + 8], 11, -2022574463)
+	      c = md5hh(c, d, a, b, x[i + 11], 16, 1839030562)
+	      b = md5hh(b, c, d, a, x[i + 14], 23, -35309556)
+	      a = md5hh(a, b, c, d, x[i + 1], 4, -1530992060)
+	      d = md5hh(d, a, b, c, x[i + 4], 11, 1272893353)
+	      c = md5hh(c, d, a, b, x[i + 7], 16, -155497632)
+	      b = md5hh(b, c, d, a, x[i + 10], 23, -1094730640)
+	      a = md5hh(a, b, c, d, x[i + 13], 4, 681279174)
+	      d = md5hh(d, a, b, c, x[i], 11, -358537222)
+	      c = md5hh(c, d, a, b, x[i + 3], 16, -722521979)
+	      b = md5hh(b, c, d, a, x[i + 6], 23, 76029189)
+	      a = md5hh(a, b, c, d, x[i + 9], 4, -640364487)
+	      d = md5hh(d, a, b, c, x[i + 12], 11, -421815835)
+	      c = md5hh(c, d, a, b, x[i + 15], 16, 530742520)
+	      b = md5hh(b, c, d, a, x[i + 2], 23, -995338651)
 
-	      a = md5_ii(a, b, c, d, x[i], 6, -198630844)
-	      d = md5_ii(d, a, b, c, x[i + 7], 10, 1126891415)
-	      c = md5_ii(c, d, a, b, x[i + 14], 15, -1416354905)
-	      b = md5_ii(b, c, d, a, x[i + 5], 21, -57434055)
-	      a = md5_ii(a, b, c, d, x[i + 12], 6, 1700485571)
-	      d = md5_ii(d, a, b, c, x[i + 3], 10, -1894986606)
-	      c = md5_ii(c, d, a, b, x[i + 10], 15, -1051523)
-	      b = md5_ii(b, c, d, a, x[i + 1], 21, -2054922799)
-	      a = md5_ii(a, b, c, d, x[i + 8], 6, 1873313359)
-	      d = md5_ii(d, a, b, c, x[i + 15], 10, -30611744)
-	      c = md5_ii(c, d, a, b, x[i + 6], 15, -1560198380)
-	      b = md5_ii(b, c, d, a, x[i + 13], 21, 1309151649)
-	      a = md5_ii(a, b, c, d, x[i + 4], 6, -145523070)
-	      d = md5_ii(d, a, b, c, x[i + 11], 10, -1120210379)
-	      c = md5_ii(c, d, a, b, x[i + 2], 15, 718787259)
-	      b = md5_ii(b, c, d, a, x[i + 9], 21, -343485551)
+	      a = md5ii(a, b, c, d, x[i], 6, -198630844)
+	      d = md5ii(d, a, b, c, x[i + 7], 10, 1126891415)
+	      c = md5ii(c, d, a, b, x[i + 14], 15, -1416354905)
+	      b = md5ii(b, c, d, a, x[i + 5], 21, -57434055)
+	      a = md5ii(a, b, c, d, x[i + 12], 6, 1700485571)
+	      d = md5ii(d, a, b, c, x[i + 3], 10, -1894986606)
+	      c = md5ii(c, d, a, b, x[i + 10], 15, -1051523)
+	      b = md5ii(b, c, d, a, x[i + 1], 21, -2054922799)
+	      a = md5ii(a, b, c, d, x[i + 8], 6, 1873313359)
+	      d = md5ii(d, a, b, c, x[i + 15], 10, -30611744)
+	      c = md5ii(c, d, a, b, x[i + 6], 15, -1560198380)
+	      b = md5ii(b, c, d, a, x[i + 13], 21, 1309151649)
+	      a = md5ii(a, b, c, d, x[i + 4], 6, -145523070)
+	      d = md5ii(d, a, b, c, x[i + 11], 10, -1120210379)
+	      c = md5ii(c, d, a, b, x[i + 2], 15, 718787259)
+	      b = md5ii(b, c, d, a, x[i + 9], 21, -343485551)
 
-	      a = safe_add(a, olda)
-	      b = safe_add(b, oldb)
-	      c = safe_add(c, oldc)
-	      d = safe_add(d, oldd)
+	      a = safeAdd(a, olda)
+	      b = safeAdd(b, oldb)
+	      c = safeAdd(c, oldc)
+	      d = safeAdd(d, oldd)
 	    }
 	    return [a, b, c, d]
 	  }
@@ -1142,14 +1149,14 @@
 	  /*
 	  * Calculate the MD5 of a raw string
 	  */
-	  function rstr_md5 (s) {
-	    return binl2rstr(binl_md5(rstr2binl(s), s.length * 8))
+	  function rstrMD5 (s) {
+	    return binl2rstr(binlMD5(rstr2binl(s), s.length * 8))
 	  }
 
 	  /*
 	  * Calculate the HMAC-MD5, of a key and some data (raw strings)
 	  */
-	  function rstr_hmac_md5 (key, data) {
+	  function rstrHMACMD5 (key, data) {
 	    var i
 	    var bkey = rstr2binl(key)
 	    var ipad = []
@@ -1157,28 +1164,28 @@
 	    var hash
 	    ipad[15] = opad[15] = undefined
 	    if (bkey.length > 16) {
-	      bkey = binl_md5(bkey, key.length * 8)
+	      bkey = binlMD5(bkey, key.length * 8)
 	    }
 	    for (i = 0; i < 16; i += 1) {
 	      ipad[i] = bkey[i] ^ 0x36363636
 	      opad[i] = bkey[i] ^ 0x5C5C5C5C
 	    }
-	    hash = binl_md5(ipad.concat(rstr2binl(data)), 512 + data.length * 8)
-	    return binl2rstr(binl_md5(opad.concat(hash), 512 + 128))
+	    hash = binlMD5(ipad.concat(rstr2binl(data)), 512 + data.length * 8)
+	    return binl2rstr(binlMD5(opad.concat(hash), 512 + 128))
 	  }
 
 	  /*
 	  * Convert a raw string to a hex string
 	  */
 	  function rstr2hex (input) {
-	    var hex_tab = '0123456789abcdef'
+	    var hexTab = '0123456789abcdef'
 	    var output = ''
 	    var x
 	    var i
 	    for (i = 0; i < input.length; i += 1) {
 	      x = input.charCodeAt(i)
-	      output += hex_tab.charAt((x >>> 4) & 0x0F) +
-	      hex_tab.charAt(x & 0x0F)
+	      output += hexTab.charAt((x >>> 4) & 0x0F) +
+	      hexTab.charAt(x & 0x0F)
 	    }
 	    return output
 	  }
@@ -1186,37 +1193,37 @@
 	  /*
 	  * Encode a string as utf-8
 	  */
-	  function str2rstr_utf8 (input) {
+	  function str2rstrUTF8 (input) {
 	    return unescape(encodeURIComponent(input))
 	  }
 
 	  /*
 	  * Take string arguments and return either raw or hex encoded strings
 	  */
-	  function raw_md5 (s) {
-	    return rstr_md5(str2rstr_utf8(s))
+	  function rawMD5 (s) {
+	    return rstrMD5(str2rstrUTF8(s))
 	  }
-	  function hex_md5 (s) {
-	    return rstr2hex(raw_md5(s))
+	  function hexMD5 (s) {
+	    return rstr2hex(rawMD5(s))
 	  }
-	  function raw_hmac_md5 (k, d) {
-	    return rstr_hmac_md5(str2rstr_utf8(k), str2rstr_utf8(d))
+	  function rawHMACMD5 (k, d) {
+	    return rstrHMACMD5(str2rstrUTF8(k), str2rstrUTF8(d))
 	  }
-	  function hex_hmac_md5 (k, d) {
-	    return rstr2hex(raw_hmac_md5(k, d))
+	  function hexHMACMD5 (k, d) {
+	    return rstr2hex(rawHMACMD5(k, d))
 	  }
 
 	  function md5 (string, key, raw) {
 	    if (!key) {
 	      if (!raw) {
-	        return hex_md5(string)
+	        return hexMD5(string)
 	      }
-	      return raw_md5(string)
+	      return rawMD5(string)
 	    }
 	    if (!raw) {
-	      return hex_hmac_md5(key, string)
+	      return hexHMACMD5(key, string)
 	    }
-	    return raw_hmac_md5(key, string)
+	    return rawHMACMD5(key, string)
 	  }
 
 	  if (true) {
